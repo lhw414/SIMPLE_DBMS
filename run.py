69f709e976d34a3b05a69db27baae37915be06a1
lhw414 <dlgusdn0414@snu.ac.kr>
2023-04-12 16:17:39 +0900

    Refactor : extract sqltranformer class

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import lark
+from sqlTransformer import SqlTransformer
 from berkeleydb import db
 import sql_exception
 
@@ -10,68 +11,8 @@ PRJ 1-1 : SQL PARSER
  - Logic : For loop, get Sql input and parsing it by sqlParser, decide its type using sqlTransformer
 
 PRJ 1-2 : Implementing DDL & Basic DML Function
-
+ - Refactor : Extract SqlTransformer class to sqlTransformer.py
 """
-
-# Class : Using lark.transformer, bottom-up query searching 
-class SqlTransformer(lark.Transformer):
-    def __init__(self):
-        self.sql_type = ""
-
-    # Uppermost query
-    def command(self, args):
-        if args[0] == "exit":
-            return "exit"
-        else:
-            return self.sql_type
-
-    def query_list(self, args):
-        return args
-
-    def query(self, args):
-        return args
-
-    # Decide sql_type
-    def create_table_query(self, args):
-        self.sql_type = "CREATE TABLE"
-        table_name = args[2].children[0].lower()
-        return
-
-    def drop_table_query(self, args):
-        self.sql_type = "DROP TABLE"
-        return
-
-    def exlpain_table_query(self, args):
-        self.sql_type = "EXPLAIN"
-        return
-
-    def describe_table_query(self, args):
-        self.sql_type = "DESCRIBE"
-        return
-
-    def desc_table_query(self, args):
-        self.sql_type = "DESC"
-        return
-
-    def insert_query(self, args):
-        self.sql_type = "INSERT"
-        return
-
-    def delete_query(self, args):
-        self.sql_type = "DELETE"
-        return
-
-    def select_query(self, args):
-        self.sql_type = "SELECT"
-        return
-
-    def show_table_query(self, args):
-        self.sql_type = "SHOW TABLES"
-        return
-
-    def update_table_query(self, args):
-        self.sql_type = "UPDATE"
-        return
 
 # SQL Parser using grammar.lark
 with open("grammar.lark") as file:
---
+++ sqlTransformer.py
@@ -0,0 +1,67 @@
+""" 
+sqlTransformer.py
+ - define SqlTransformer class for bottom up query searching
+
+ Class : Using lark.transformer, bottom-up query searching 
+"""
+
+import lark
+
+class SqlTransformer(lark.Transformer):
+    def __init__(self):
+        self.sql_type = ""
+
+    # Uppermost query
+    def command(self, args):
+        if args[0] == "exit":
+            return "exit"
+        else:
+            return self.sql_type
+
+    def query_list(self, args):
+        return args
+
+    def query(self, args):
+        return args
+
+    # Decide sql_type
+    def create_table_query(self, args):
+        self.sql_type = "CREATE TABLE"
+        table_name = args[2].children[0].lower()
+        return
+
+    def drop_table_query(self, args):
+        self.sql_type = "DROP TABLE"
+        return
+
+    def exlpain_table_query(self, args):
+        self.sql_type = "EXPLAIN"
+        return
+
+    def describe_table_query(self, args):
+        self.sql_type = "DESCRIBE"
+        return
+
+    def desc_table_query(self, args):
+        self.sql_type = "DESC"
+        return
+
+    def insert_query(self, args):
+        self.sql_type = "INSERT"
+        return
+
+    def delete_query(self, args):
+        self.sql_type = "DELETE"
+        return
+
+    def select_query(self, args):
+        self.sql_type = "SELECT"
+        return
+
+    def show_table_query(self, args):
+        self.sql_type = "SHOW TABLES"
+        return
+
+    def update_table_query(self, args):
+        self.sql_type = "UPDATE"
+        return
